83576577e46879f5be358deb8a59295c2f4c0f72
diegoG82 <diegogiardina@hotmail.it>
2023-03-15 13:10:27 +0100

    Project Scaffolding

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+// Rivediamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+
+
